c9da17f154ac52772ee82c8cc55a5f5661869c4f
Gene Kelley <eugenek@guildmortgage.net>
2020-07-28 11:33:14 -0700

    Add Foobar controller, model, migration and seeder

---
+++ database/factories/FoobarFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Foobar;
+use Faker\Generator as Faker;
+
+$factory->define(Foobar::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
