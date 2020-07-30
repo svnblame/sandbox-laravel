58f79370549f2a930cd08a9a6058422905b71c27
Gene Kelley <eugenek@guildmortgage.net>
2020-07-30 12:26:24 -0700

    Install and play around with eslint.

---
+++ resources/assets/js/components/Replies.vue
@@ -0,0 +1,83 @@
+<template>
+  <div
+    class="flex"
+    style="margin-left: 56px"
+  >
+    <div>
+      <div
+        v-for="(reply, index) in items"
+        :key="reply.id"
+      >
+        <reply
+          :reply="reply"
+          @deleted="remove(index)"
+        />
+      </div>
+
+      <paginator
+        :data-set="dataSet"
+        @changed="fetch"
+      />
+
+      <p
+        v-if="$parent.locked"
+        class="mt-4 text-sm text-grey-dark text-center"
+      >
+        This thread has been locked. No more replies are allowed.
+      </p>
+
+      <new-reply
+        v-else
+        @created="add"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Reply from "./Reply.vue";
+import NewReply from "./NewReply.vue";
+import collection from "../mixins/collection";
+
+export default {
+    components: { Reply, NewReply },
+
+    mixins: [collection],
+
+    data() {
+        return { dataSet: false };
+    },
+
+    created() {
+        this.fetch();
+    },
+
+    methods: {
+        fetch(page) {
+            if (page === 'foo') {
+                var re = /foo\s{4}bar/;
+                console.log(re);
+            }
+
+            window.axios.get(this.url(page)).then(this.refresh);
+        },
+
+        url(page) {
+            if (!page) {
+                let query = location.search.match(/page=(\d+)/);
+
+                page = query ? query[1] : 1;
+            }
+
+            return `${location.pathname}/replies?page=${page}`;
+        },
+
+        refresh({ data }) {
+            this.dataSet = data;
+            this.items = data.data;
+
+            window.scrollTo(0, 0);
+        }
+    }
+};
+</script>
