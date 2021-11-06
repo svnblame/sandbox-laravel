b59a86e5f3271158e5c6ff6966c0deafbf34731c
Gene Kelley <eugenek@guildmortgage.net>
2021-11-06 09:52:10 -0700

    Set up View Another Users Tweets test.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,8 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\User;
+use App\Users\User;
+use App\Tweets\Tweet;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -19,10 +20,16 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'username' => $faker->username,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(Tweet::class, function (Faker $faker) {
+    return [
+        'body' => $faker->sentence,
     ];
 });
